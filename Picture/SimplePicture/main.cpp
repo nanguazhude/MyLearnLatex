853fa29a4dea31783410c202fff324f8660e0a51
nanguazhude <nanguazhude@vip.qq.com>
2018-03-19 21:41:34 +0800

    +test code

--- Picture/SimplePicture/main.cpp
+++ Picture/SimplePicture/main.cpp
@@ -6,24 +6,105 @@
 #include <QtGui/QtGui>
 #include <optional>
 #include <fstream>
-using std::pmr::vector;
+#include <list>
+#include <map>
+#include <string>
+#include <string_view>
 
-inline QString operator""_qu8(const char * a,std::size_t b) {
+using std::pmr::vector;
+using std::pmr::list;
+using namespace std::string_view_literals;
+
+inline QString operator""_qu8(const char * a, std::size_t b) {
 	return QString::fromUtf8(a, static_cast<int>(b));
 }
 
 inline std::ofstream & log() {
-	static std::ofstream varStream{"log.txt",std::ios::out};
+	static std::ofstream varStream{ "log.txt",std::ios::out };
 	return varStream;
+}
+
+inline QString toVerb(const QString & arg) {
+	//arg.replace(" "_qu8, "_"_qu8);
+	if constexpr(false) {
+		static const std::pmr::map<char, std::string_view > KeyMap = []() {
+			std::pmr::map<char, std::string_view > varAns;
+			varAns.emplace('#', u8R"(\# )"sv);
+			varAns.emplace('$', u8R"(\$ )"sv);
+			varAns.emplace('%', u8R"(\% )"sv);
+			varAns.emplace('^', u8R"(\^{} )"sv);
+			varAns.emplace('&', u8R"(\& )"sv);
+			varAns.emplace('_', u8R"(\_ )"sv);
+			varAns.emplace('{', u8R"(\{ )"sv);
+			varAns.emplace('}', u8R"(\} )"sv);
+			varAns.emplace('\\', u8R"(\textbackslash )"sv);
+			varAns.emplace('~', u8R"(\~{} )"sv);
+			return std::move(varAns);
+		}();
+
+		const auto var8 = arg.toUtf8();
+		std::pmr::vector<char> varTmp;
+
+		const auto varE = KeyMap.end();
+		for (const auto & varI : var8) {
+			auto varJ = KeyMap.find(varI);
+			if (varJ == varE) {
+				varTmp.push_back(varI);
+			}
+			else {
+				varTmp.insert(varTmp.end(), varJ->second.begin(), varJ->second.end());
+			}
+		}
+
+		return QString::fromUtf8(varTmp.data(), static_cast<int>(varTmp.size()));
+	}
+	else {
+		static const std::pmr::map< char16_t, std::u16string_view > KeyMap = []() {
+			std::pmr::map<char16_t, std::u16string_view > varAns;
+			varAns.emplace(u'#', uR"(\# )"sv);
+			varAns.emplace(u'$', uR"(\$ )"sv);
+			varAns.emplace(u'%', uR"(\% )"sv);
+			varAns.emplace(u'^', uR"(\^{} )"sv);
+			varAns.emplace(u'&', uR"(\& )"sv);
+			varAns.emplace(u'_', uR"(\_ )"sv);
+			varAns.emplace(u'{', uR"(\{ )"sv);
+			varAns.emplace(u'}', uR"(\} )"sv);
+			varAns.emplace(u'\\', uR"(\textbackslash )"sv);
+			varAns.emplace(u'~', uR"(\~{} )"sv);
+			return std::move(varAns);
+		}();
+
+		const std::u16string_view var8{ reinterpret_cast<const char16_t  *>(arg.begin()),
+			static_cast<std::size_t>(arg.size()) };
+
+		std::pmr::vector<char16_t> varTmp;
+		const auto varE = KeyMap.end();
+
+		for (const auto & varI : var8) {
+			auto varJ = KeyMap.find(varI);
+			if (varJ == varE) {
+				varTmp.push_back(varI);
+			}
+			else {
+				varTmp.insert(varTmp.end(), varJ->second.begin(), varJ->second.end());
+			}
+		}
+
+		return QString::fromUtf16(varTmp.data(), static_cast<int>(varTmp.size()));
+
+	}
 }
 
 class Pack {
 public:
 	class FileInfo {
 	public:
+
 		int $Index = -1;
 		QString $IndexName;
 		QFileInfo $Info;
+		bool $PDF = false;
+		bool $ImageWith = false;
 		FileInfo() = default;
 		FileInfo(QFileInfo && arg) :$Info(std::move(arg)) {}
 	};
@@ -54,15 +135,15 @@ int main(int argc, char *argv[]) {
 
 inline void rmTmpFile(const QString & argDirName) {
 	QDirIterator varIt(argDirName,
-		QStringList() << "*.tmp" <<"*.pdf" ,
+		QStringList() << "*.jpg" << "*.png" << "*.pdf",
 		QDir::NoFilter,
 		QDirIterator::Subdirectories);
 	vector<QString> varDirNameAboutToDelete;
 	while (varIt.hasNext()) {
 		varIt.next();
-		varDirNameAboutToDelete.push_back( varIt.fileInfo().absoluteFilePath());
+		varDirNameAboutToDelete.push_back(varIt.fileInfo().absoluteFilePath());
 	}
-	for (const auto & varI:varDirNameAboutToDelete) {
+	for (const auto & varI : varDirNameAboutToDelete) {
 		QFile::remove(varI);
 	}
 }
@@ -92,7 +173,7 @@ inline void Pack::creakTex() {
 		if (0 != varI.$Info.suffix().compare(QLatin1String("pdf", 3), Qt::CaseInsensitive)) {
 			const QString varImageFileName = varI.$Info.absoluteFilePath();
 			QImage varImage{ varImageFileName };
-			if (varImage.isNull()) {
+			if ((varImage.isNull()) || (varImage.height() < 1)) {
 				const auto varFileName8 = varImageFileName.toLocal8Bit();
 				log()
 					<< "Image File Read Error! "
@@ -100,32 +181,42 @@ inline void Pack::creakTex() {
 					<< std::endl;
 				continue;
 			}
+
+			{
+				constexpr const static auto K = 1.414;
+				const auto k = double(varImage.width()) / double(varImage.height());
+				varI.$ImageWith = (k > K);
+			}
+
 			varI.$Index = ++varIndex;
 			varI.$IndexName = QString("%1").arg(varI.$Index, 10, 10, QChar('0'))
-				+ QLatin1String(".tmp", 4);
-			if (varImage.save(varDir->filePath(varI.$IndexName), "png") == false) {
+				+ QLatin1String(".jpg", 4);
+			if (varImage.save(varDir->filePath(varI.$IndexName), "jpg") == false) {
 				varI.$Index = -1;
 			}
+
+			varI.$PDF = false;
 		}
 		else {
 			const QString varPdfFileName = varI.$Info.absoluteFilePath();
 			varI.$Index = ++varIndex;
 			varI.$IndexName = QString("%1").arg(varI.$Index, 10, 10, QChar('0'))
-				+ QLatin1String(".tmp", 4);
+				+ QLatin1String(".pdf", 4);
 			if (false == QFile::copy(varPdfFileName, varDir->filePath(varI.$IndexName))) {
 				varI.$Index = -1;
 			}
-		}			
+			varI.$PDF = true;
+		}
 
 	}
 
 	QFile varFile{ varDir->absoluteFilePath("index.tex") };
-	if (varFile.open(QIODevice::WriteOnly)==false) {
+	if (varFile.open(QIODevice::WriteOnly) == false) {
 		log() << "create file fail!" << std::endl;
 		return;
 	}
-	QTextStream varStream{&varFile};
-	varStream.setCodec( QTextCodec::codecForName("utf8") );
+	QTextStream varStream{ &varFile };
+	varStream.setCodec(QTextCodec::codecForName("UTF-8"));
 	varStream.setGenerateByteOrderMark(false);
 
 	varStream << u8R"!!(
@@ -172,60 +263,177 @@ inline void Pack::creakTex() {
 
 \begin{document}
  
+
+
+)!!"_qu8;
+
+
+	bool isFirst = true;
+	for (auto & varI : $Files) {
+		if (varI.$Index < 0) {
+			continue;
+		}
+
+		if (isFirst) {
+			varStream << u8R"(
 %280X190
  \noindent
  \parbox[c][\textheight][c]{\textwidth}{
  
 \begin{center} 
-
-)!!"_qu8;
-
-	for (auto & varI : $Files) {
-		if (varI.$Index < 0) {
-			continue; 
+)"_qu8;
+			isFirst = false;
+		}
+		else {
+			isFirst = true;
 		}
 
 		const auto varKeyName = varI.$Info.baseName();
 
-		const auto varBasic = u8R"!!( %1 \\
+		const static auto varBasicPdf = u8R"!!( %1 \\
 \noindent
 \resizebox{!}{14cm}{
-\rotatebox[origin=c]{ 0 }{
-	\includegraphics[width=19cm]{%2}
-}
+%\rotatebox[origin=c]{ 0 }{
+%	\includegraphics[width=19cm ]{%2}
+%}
+\includegraphics{%2}
+} \\
+)!!"_qu8;
+		//natwidth=610,natheight=642
+		const static auto varBasicImage = u8R"!!( %1 \\
+\noindent
+\resizebox{!}{14cm}{
+%\rotatebox[origin=c]{ 0 }{
+%	\includegraphics[width=19cm  ]{%2}
+%}
+\includegraphics{%2}
+} \\
+)!!"_qu8;
+		const static auto varBasicImageW = u8R"!!( %1 \\
+\noindent
+\resizebox{\textwidth}{!}{
+%\rotatebox[origin=c]{ 0 }{
+%	\includegraphics[width=19cm  ]{%2}
+%}
+\includegraphics{%2}
 } \\
 )!!"_qu8;
 
-		const auto varData = varBasic.arg(varKeyName).arg( varI.$IndexName );
-		{
-			const auto varDT = varData.toUtf8();
-			varStream << varDT.data();
+		QString varData;
+		if (varI.$PDF) {
+			varData = varBasicPdf
+				.arg(toVerb(varKeyName))
+				.arg(varI.$IndexName);
 		}
-	}
+		else {
+			if (varI.$ImageWith) {
+				varData = varBasicImageW
+					.arg(toVerb(varKeyName))
+					.arg(varI.$IndexName);
+			}
+			else {
+				varData = varBasicImage
+					.arg(toVerb(varKeyName))
+					.arg(varI.$IndexName);
+			}
+		}
 
-	varStream << u8R"!!(
+		{
+			varStream << std::as_const(varData);
+		}
+
+		if (isFirst) {
+			varStream << u8R"(
 \end{center}
 
 }
 
 \newpage 
+)"_qu8;
+		}
 
- 
+
+	}//for
+
+	varStream << u8R"!!(
+
 \end{document}
 
 )!!"_qu8;
 }
 
-inline void Pack::makeFiles() {
-	QDirIterator varIt($Dirname,
-		QStringList() << "*.jpg" << "*.jpeg" << "*.png" << "*.bmp" << "*.pdf",
-		QDir::NoFilter,
-		QDirIterator::Subdirectories);
-	$Files.clear();
-	while (varIt.hasNext()) {
-		const auto varL8 = varIt.next().toLocal8Bit();
-		$Files.emplace_back(varIt.fileInfo());
-		log() << "find a name : " << varL8.data() << std::endl;
+namespace {
+	namespace DirIterator {
+		class Next {
+		public:
+			list< QFileInfo > $Next;
+			vector< QFileInfo > $Ans;
+			Next(const QString & argDirName) {
+				QDir varDir{ argDirName };
+				const auto varList = varDir.entryInfoList(QDir::Dirs |
+					QDir::Files |
+					QDir::NoSymLinks |
+					QDir::NoDotAndDotDot);
+				for (const auto & varI : varList) {
+					if (varI.isDir()) {
+						const auto varDirNameX = varI.fileName();
+						if (varDirNameX.compare(QLatin1Literal("tmp", 3),
+							Qt::CaseInsensitive) == 0) {
+							continue;
+						}
+						$Next.push_back(varI);
+					}
+					else {
+						_add_to_ans(varI);
+					}
+				}
+				_deep_search();
+			}
+		private:
+			void _add_to_ans(const QFileInfo & arg) {
+				const auto varSuffix = arg.suffix().toLower();
+				const static QRegularExpression varExp{ u8R"(jpeg|png|jpg|pdf)"_qu8 };
+				if (varExp.match(varSuffix).hasMatch()) {
+					$Ans.push_back(arg);
+				}
+			}
+			void _deep_search() {
+				while (false == $Next.empty()) {
+					const QFileInfo varInfo = std::move($Next.front());
+					$Next.pop_front();
+					const auto varDirName = varInfo.absoluteFilePath();
+					const auto varList = QDir(varDirName)
+						.entryInfoList(QDir::Dirs |
+							QDir::Files |
+							QDir::NoSymLinks |
+							QDir::NoDotAndDotDot);
+					for (const auto & varI : varList) {
+						if (varI.isDir()) {
+							$Next.push_back(varI);
+						}
+						else {
+							_add_to_ans(varI);
+						}
+					}
+				}
+			}
+		};
 	}
 }
+
+inline void Pack::makeFiles() {
+
+	$Files.clear();
+
+	DirIterator::Next varNext{ $Dirname };
+	{
+		for (auto & varI : varNext.$Ans) {
+			const auto varL8 = varI.absoluteFilePath().toLocal8Bit();
+			log() << "find a name : " << varL8.data() << std::endl;
+			$Files.emplace_back(std::move(varI));
+		}
+	}
+
+}
+
 
